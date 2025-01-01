9df38618082457d3947043a2fc46c553910a5647
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2025-01-02 02:06:32 +0330

    Initial Entry

---
+++ MotorDriver_DCMotor_SpeedAndDirectionControllerWithRF_ASK/Code_BascomAVR/v1.0.bas
@@ -0,0 +1,166 @@
+'GitHub Account: GitHub.com/AliRezaJoodi
+
+$regfile = "m16def.dat"
+$crystal = 8000000
+
+Config Lcd = 16 * 2
+Config Lcdpin = Pin , Db4 = Porta.4 , Db5 = Porta.5 , Db6 = Porta.6 , Db7 = Porta.7 , E = Porta.2 , Rs = Porta.0
+Cursor Off
+Cls
+
+Config Watchdog = 256
+
+Enable Interrupts
+
+Config Timer1 = Pwm , Pwm = 8 , Compare A Pwm = Clear Down , Compare B Pwm = Disconnect , Prescale = 64
+'Start Timer1
+Pwm1a = 0
+
+Enable Interrupts
+Config Int0 = Rising
+On Int0 T4
+Enable Int0
+
+Config Portb = Input
+Config Portd.2 = Input
+
+
+Config Portc.0 = Output : Portc.0 = 0 : Relay Alias Portc.0
+Config Portb.4 = Output : Portb.4 = 0 : Led Alias Portb.4
+
+Config Porta.3 = Input
+
+Config Portc.6 = Input
+Config Portc.7 = Input
+
+Dim Pwm_motor As Byte
+Dim Pwm_motor_eeprom As Eram Byte
+
+Dim I As Word
+Dim Status As Byte : Status = 0
+Dim B As Byte
+
+Start Watchdog
+
+Gosub Eeprom_load : Reset Watchdog
+Gosub Display_lcd : Reset Watchdog
+Gosub Driver : Reset Watchdog
+
+Do
+   Reset Watchdog
+Loop
+
+End
+
+
+'********************************************
+T4:
+   Do
+      Set Led
+      Gosub Red_key
+
+      Select Case B
+         Case 8:
+            Gosub Left_motor
+            Gosub Display_lcd
+            Gosub Driver
+         Case 1:
+            Gosub Right_motor
+            Gosub Display_lcd
+            Gosub Driver
+         Case 4:
+            Gosub Stop_motor
+            Gosub Display_lcd
+            Gosub Driver
+         Case 2:
+            Incr I
+            If I = 11000 Then
+               Gosub Up_motor
+               Gosub Eeprom_save
+               Gosub Display_lcd
+               Gosub Driver
+               I = 0
+            End If
+         Case 3:
+            Incr I
+            If I = 11000 Then
+               Gosub Down_motor
+               Gosub Eeprom_save
+               Gosub Display_lcd
+               Gosub Driver
+               I = 0
+            End If
+      End Select
+      Reset Watchdog
+   Loop Until Pind.2 = 0
+   B = 0 : Reset Led
+Return
+
+'********************************************
+Red_key:
+   B.0 = Pinb.0
+   B.1 = Pinb.1
+   B.2 = Pinb.2
+   B.3 = Pinb.3
+   B = B And &B00001111
+Return
+
+'********************************************
+Display_lcd:
+   If Status = 0 Then
+      Locate 1 , 1 : Lcd "Stop Motor   "
+   Elseif Status = 1 Then
+      Locate 1 , 1 : Lcd "Rotate Left  "
+   Elseif Status = 2 Then
+      Locate 1 , 1 : Lcd "Rotate Right "
+   End If
+   Locate 2 , 1 : Lcd "PWM= " ; Pwm_motor ; "        "
+Return
+
+'********************************************
+Up_motor:
+   Pwm_motor = Pwm_motor + 5
+   If Pwm_motor = 4 Then Pwm_motor = 255
+Return
+
+'********************************************
+Down_motor:
+   Pwm_motor = Pwm_motor - 5
+   If Pwm_motor = 251 Then Pwm_motor = 0
+Return
+
+'********************************************
+Stop_motor:
+   Status = 0
+Return
+
+'********************************************
+Left_motor:
+   Status = 1
+Return
+
+'********************************************
+Right_motor:
+   Status = 2
+Return
+
+'********************************************
+Driver:
+   If Status = 0 Then
+      Pwm1a = 0
+   Elseif Status = 1 Then
+      Set Relay : Waitms 10 : Pwm1a = Pwm_motor
+   Elseif Status = 2 Then
+      Reset Relay : Waitms 10 : Pwm1a = Pwm_motor
+   End If
+Return
+
+'********************************************
+Eeprom_save:
+   Pwm_motor_eeprom = Pwm_motor
+Return
+
+'********************************************
+Eeprom_load:
+   Pwm_motor = Pwm_motor_eeprom
+Return
---
+++ MotorDriver_DCMotor_SpeedAndDirectionControllerWithRF_ASK/Code_CodeVisionAVR/v1.0.c
@@ -0,0 +1,187 @@
+// GitHub Account: GitHub.com/AliRezaJoodi
+
+#include <mega16.h>
+#include <lcd.h>
+#include <delay.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// Alphanumeric LCD Module functions
+#asm
+   .equ __lcd_port=0x1B ;PORTA
+#endasm
+#include <lcd.h>
+
+#define SETBIT(ADDRESS,BIT)  (ADDRESS|=1<<BIT)
+#define CLRBIT(ADDRESS,BIT)  (ADDRESS &=~(1<<BIT))
+#define RELAY PORTC.0
+#define LED PORTB.4
+
+void Configuration_LCD(void);
+void EEPROM_Load(void);
+void EEPROM_Save(void);
+void Start_Sub(void);
+void Motor_UP(void);
+void Motor_Down(void);
+void Motor_Left(void);
+void Motor_Right(void);
+void Motor_Stop(void);
+
+unsigned char PWM_Motor;
+eeprom unsigned char PWM_Motor_EEPROM; 
+bit Status_Motor;
+char buffer[16];
+unsigned char key;
+unsigned int t;
+
+interrupt [EXT_INT0] void ext_int0_isr(void){
+    key=0;
+    do{
+        //LED=1;
+        if(PINB.0==1){SETBIT(key,0);} else if(PINB.0==0){CLRBIT(key,0);}
+        if(PINB.1==1){SETBIT(key,1);} else if(PINB.1==0){CLRBIT(key,1);}
+        if(PINB.2==1){SETBIT(key,2);} else if(PINB.2==0){CLRBIT(key,2);}
+        if(PINB.3==1){SETBIT(key,3);} else if(PINB.3==0){CLRBIT(key,3);}
+        //lcd_gotoxy(0,1); lcd_putsf("key= "); 
+        //itoa(key,buffer); lcd_puts(buffer); lcd_putsf("    "); 
+        //delay_ms(200);
+        if(key==8){Motor_Left(); t=0;}
+        if(key==1){Motor_Right(); t=0;} 
+        if(key==4){Motor_Stop(); t=0;} 
+        if(key==2){
+            t=t+1; if(t>=16000){t=0; Motor_UP();}
+        } 
+        if(key==3){
+            t=t+1; if(t>=16000){t=0; Motor_Down();}
+        }        
+    }
+    while (PIND.2==1);
+    LED=0;   
+}
+
+void main(void){
+
+    // External Interrupt(s) initialization
+    // INT0: On
+    // INT0 Mode: Rising Edge
+    // INT1: Off
+    // INT2: Off
+    GICR|=0x40;
+    MCUCR=0x03;
+    MCUCSR=0x00;
+    GIFR=0x40;
+
+// Timer/Counter 1 initialization
+// Clock source: System Clock
+// Clock value: 125.000 kHz
+// Mode: CTC top=OCR1A
+// OC1A output: Toggle
+// OC1B output: Discon.
+// Noise Canceler: Off
+// Input Capture on Falling Edge
+// Timer 1 Overflow Interrupt: Off
+// Input Capture Interrupt: Off
+// Compare A Match Interrupt: Off
+// Compare B Match Interrupt: Off
+TCCR1A=0x40;
+TCCR1B=0x0B;
+TCNT1H=0x00;
+TCNT1L=0x00;
+ICR1H=0x00;
+ICR1L=0x00;
+OCR1AH=0x00;
+OCR1AL=0x00;
+OCR1BH=0x00;
+OCR1BL=0x00;
+
+TIMSK=0x00;
+
+//TCCR1A=0x40;
+//TCCR1B=0x0A;
+//OCR1AH=0x01;
+//OCR1AL=0xF3;
+
+DDRD.5=1; PORTD.5=0;
+
+    DDRC.0=1; PORTC.0=0;
+    DDRB.4=1; PORTB.4=0;
+    DDRB.0=0; PORTB.0=1;
+    DDRB.1=0; PORTB.1=1;
+    DDRB.2=0; PORTB.2=1;
+    DDRB.3=0; PORTB.3=1;
+    
+   
+    #asm("sei")  // Global enable interrupts
+
+    Configuration_LCD();
+    EEPROM_Load();
+    Start_Sub();
+    
+    while (1){
+        //Motor_Down(); delay_ms(400);
+        //LED=0;
+    }
+}
+
+//********************************************************
+void Configuration_LCD(void){
+    lcd_init(16); lcd_clear();   
+}
+
+//********************************************************
+void Start_Sub(void){    
+    lcd_clear(); 
+    lcd_gotoxy(0,0); lcd_putsf("Stop Motor");
+    lcd_gotoxy(0,1); lcd_putsf("PWM= "); 
+    itoa(PWM_Motor,buffer); lcd_puts(buffer);         
+}
+
+//********************************************************
+void Motor_UP(void){
+    PWM_Motor=PWM_Motor+5; 
+    if(PWM_Motor==4){PWM_Motor=0;}
+    lcd_gotoxy(0,1); lcd_putsf("PWM= "); itoa(PWM_Motor,buffer); lcd_puts(buffer); lcd_putsf("        ");
+    PWM_Motor_EEPROM=PWM_Motor; delay_ms(10);
+}
+
+//********************************************************
+void Motor_Down(void){
+    PWM_Motor=PWM_Motor-5; 
+    if(PWM_Motor==251){PWM_Motor=255;}
+    lcd_gotoxy(0,1); lcd_putsf("PWM= "); itoa(PWM_Motor,buffer); lcd_puts(buffer); lcd_putsf("        ");
+    PWM_Motor_EEPROM=PWM_Motor; delay_ms(10);
+}
+
+//********************************************************
+void Motor_Left(void){
+    RELAY=1;
+    Status_Motor=1;
+    lcd_gotoxy(0,0); lcd_putsf("Rotate Left  ");
+    lcd_gotoxy(0,1); lcd_putsf("PWM= "); 
+    itoa(PWM_Motor,buffer); lcd_puts(buffer);      
+}
+
+//********************************************************
+void Motor_Right(void){
+    RELAY=0;
+    Status_Motor=1;
+    lcd_gotoxy(0,0); lcd_putsf("Rotate Right  ");
+    lcd_gotoxy(0,1); lcd_putsf("PWM= "); 
+    itoa(PWM_Motor,buffer); lcd_puts(buffer);  
+}
+
+//********************************************************
+void Motor_Stop(void){
+    Status_Motor=0;
+    lcd_gotoxy(0,0); lcd_putsf("Stop Motor      ");    
+}
+
+//********************************************************
+void EEPROM_Load(void){
+    PWM_Motor=PWM_Motor_EEPROM; delay_ms(10);
+}
+
+//********************************************************
+void EEPROM_Save(void){
+    PWM_Motor_EEPROM=PWM_Motor; delay_ms(10);
+}
