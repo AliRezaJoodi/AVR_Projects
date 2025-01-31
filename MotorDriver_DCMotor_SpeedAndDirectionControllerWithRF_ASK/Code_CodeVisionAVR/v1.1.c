b0f58ecff55d996d2f217dac0e072f36d4c78950
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2025-01-31 21:35:26 +0330

    Update

--- MotorDriver_DCMotor_SpeedAndDirectionControllerWithRF_ASK/Code_CodeVisionAVR/v1.1.c
+++ MotorDriver_DCMotor_SpeedAndDirectionControllerWithRF_ASK/Code_CodeVisionAVR/v1.1.c
@@ -28,7 +28,7 @@ void Motor_Left(void);
 void Motor_Right(void);
 void Motor_Stop(void);
 
-unsigned char PWM_Motor;
+unsigned char motor_pwm;
 eeprom unsigned char PWM_Motor_EEPROM; 
 bit Status_Motor;
 bit int_task=0;
@@ -73,20 +73,24 @@ void main(void){
     MCUCSR=0x00;
     GIFR=0x40;
 
+
 // Timer/Counter 1 initialization
 // Clock source: System Clock
 // Clock value: 125.000 kHz
-// Mode: CTC top=OCR1A
-// OC1A output: Toggle
-// OC1B output: Discon.
+// Mode: Fast PWM top=0x00FF
+// OC1A output: Non-Inverted PWM
+// OC1B output: Disconnected
 // Noise Canceler: Off
 // Input Capture on Falling Edge
-// Timer 1 Overflow Interrupt: Off
+// Timer Period: 2.048 ms
+// Output Pulse(s):
+// OC1A Period: 2.048 ms Width: 0 us
+// Timer1 Overflow Interrupt: Off
 // Input Capture Interrupt: Off
 // Compare A Match Interrupt: Off
 // Compare B Match Interrupt: Off
-TCCR1A=0x40;
-TCCR1B=0x0B;
+TCCR1A=(1<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<WGM11) | (1<<WGM10);
+TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (1<<WGM12) | (0<<CS12) | (1<<CS11) | (1<<CS10);
 TCNT1H=0x00;
 TCNT1L=0x00;
 ICR1H=0x00;
@@ -114,22 +118,25 @@ DDRD.5=1; PORTD.5=0;
     DDRB.3=0; PORTB.3=1;
     
     DDRD.2=0;
-   
+    
+    DDRD.5=1; PORTD.5=0;
+    
     #asm("sei")  // Global enable interrupts
 
     LCD_Config();
     EEPROM_Default();
-    EEPROM_Load();
+    EEPROM_Load(); 
+    OCR1A=motor_pwm; 
     Start_Sub();
     
     while (1){
         //Motor_Down(); delay_ms(400);
         //LED=0; 
         if(int_task==1){
+            LED=1;
         
                 key=0;
     do{
-        LED=1;
         if(PINB.0==1){SETBIT(key,0);} else if(PINB.0==0){CLRBIT(key,0);}
         if(PINB.1==1){SETBIT(key,1);} else if(PINB.1==0){CLRBIT(key,1);}
         if(PINB.2==1){SETBIT(key,2);} else if(PINB.2==0){CLRBIT(key,2);}
@@ -147,8 +154,8 @@ DDRD.5=1; PORTD.5=0;
             t=t+1; if(t>=16000){t=0; Motor_Down();}
         }        
     }while(PIND.2==1);
-    LED=0;
-    
+            
+            LED=0;
             int_task=0;
         }
     }
@@ -164,23 +171,23 @@ void Start_Sub(void){
     lcd_clear(); 
     lcd_gotoxy(0,0); lcd_putsf("Stop Motor");
     lcd_gotoxy(0,1); lcd_putsf("PWM= "); 
-    itoa(PWM_Motor,buffer); lcd_puts(buffer);         
+    itoa(motor_pwm,buffer); lcd_puts(buffer);         
 }
 
 //********************************************************
 void Motor_UP(void){
-    PWM_Motor=PWM_Motor+5; 
-    if(PWM_Motor==4){PWM_Motor=0;}
-    lcd_gotoxy(0,1); lcd_putsf("PWM= "); itoa(PWM_Motor,buffer); lcd_puts(buffer); lcd_putsf("        ");
-    PWM_Motor_EEPROM=PWM_Motor; delay_ms(10);
+    motor_pwm=motor_pwm+5; 
+    if(motor_pwm==4){motor_pwm=0;}
+    lcd_gotoxy(0,1); lcd_putsf("PWM= "); itoa(motor_pwm,buffer); lcd_puts(buffer); lcd_putsf("        ");
+    PWM_Motor_EEPROM=motor_pwm; delay_ms(10);
 }
 
 //********************************************************
 void Motor_Down(void){
-    PWM_Motor=PWM_Motor-5; 
-    if(PWM_Motor==251){PWM_Motor=255;}
-    lcd_gotoxy(0,1); lcd_putsf("PWM= "); itoa(PWM_Motor,buffer); lcd_puts(buffer); lcd_putsf("        ");
-    PWM_Motor_EEPROM=PWM_Motor; delay_ms(10);
+    motor_pwm=motor_pwm-5; 
+    if(motor_pwm==251){motor_pwm=255;}
+    lcd_gotoxy(0,1); lcd_putsf("PWM= "); itoa(motor_pwm,buffer); lcd_puts(buffer); lcd_putsf("        ");
+    PWM_Motor_EEPROM=motor_pwm; delay_ms(10);
 }
 
 //********************************************************
@@ -189,7 +196,7 @@ void Motor_Left(void){
     Status_Motor=1;
     lcd_gotoxy(0,0); lcd_putsf("Rotate Left  ");
     lcd_gotoxy(0,1); lcd_putsf("PWM= "); 
-    itoa(PWM_Motor,buffer); lcd_puts(buffer);      
+    itoa(motor_pwm,buffer); lcd_puts(buffer);      
 }
 
 //********************************************************
@@ -198,7 +205,7 @@ void Motor_Right(void){
     Status_Motor=1;
     lcd_gotoxy(0,0); lcd_putsf("Rotate Right  ");
     lcd_gotoxy(0,1); lcd_putsf("PWM= "); 
-    itoa(PWM_Motor,buffer); lcd_puts(buffer);  
+    itoa(motor_pwm,buffer); lcd_puts(buffer);  
 }
 
 //********************************************************
@@ -209,19 +216,19 @@ void Motor_Stop(void){
 
 //********************************************************
 void EEPROM_Load(void){
-    PWM_Motor=PWM_Motor_EEPROM;
+    motor_pwm=PWM_Motor_EEPROM;
 }
 
 //********************************************************
 void EEPROM_Save(void){
-    PWM_Motor_EEPROM=PWM_Motor;
+    PWM_Motor_EEPROM=motor_pwm;
     delay_ms(10);
 }
 
 //********************************************************
 void EEPROM_Default(void){
-    PWM_Motor=50;
-    PWM_Motor_EEPROM=PWM_Motor;
+    motor_pwm=50;
+    PWM_Motor_EEPROM=motor_pwm;
     delay_ms(10); 
-    PWM_Motor=PWM_Motor_EEPROM;
+    motor_pwm=PWM_Motor_EEPROM;
 }
